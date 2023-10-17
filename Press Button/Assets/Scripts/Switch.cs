8d9fc0ae46bd3440de3de9c3136276e49df49d55
DinosaurPower <lysym685@newschool.edu>
2023-10-16 20:27:02 -0400

    added sound effects and color switch script

---
+++ Press Button/Assets/Scripts/Switch.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour
+{
+   public Image [] sprites;
+    public bool IsOn = false;
+
+public void ColorSwitch()
+{
+    Debug.Log("Clicked");
+
+    // Toggle the switch state
+    IsOn = !IsOn;
+
+    for (int i = 0; i < sprites.Length; i++)
+    {
+        // Get the current color of the image
+        Color thisColor = sprites[i].color;
+
+        // Modify the alpha component of the color
+        if (IsOn)
+        {
+            thisColor.a = 1.0f;  // Fully visible
+        }
+        else
+        {
+            thisColor.a = 0.1f;  // Partially transparent
+        }
+
+        // Apply the modified color back to the image
+        sprites[i].color = thisColor;
+    }
+
+    
+}
+
+}
+

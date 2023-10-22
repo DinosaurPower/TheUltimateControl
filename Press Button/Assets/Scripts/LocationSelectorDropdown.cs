297c8eb968bca95183c13669a88f1c2960eae46c
DinosaurPower <lysym685@newschool.edu>
2023-10-21 23:25:32 -0400

    fixed invisible script glitch

--- Press Button/Assets/Scripts/LocationSelectorDropdown.cs
+++ Press Button/Assets/Scripts/LocationSelectorDropdown.cs
@@ -6,11 +6,22 @@ using TMPro;
 public class LocationSelectorDropdown : MonoBehaviour
 {
     public  TMP_Dropdown m_Dropdown;
+    public Tracker tracker;
 
+void Start(){
+
+
+    tracker= GetComponent <Tracker>();
+    }
 public void ChangeLocationDropDown() {
         Debug.Log("DROP DOWN CHANGED");
       
 
         Debug.Log(m_Dropdown.options[m_Dropdown.value].text);
+
+        if (m_Dropdown.options[m_Dropdown.value].text == "Option C" && tracker.SliderRevealed == true){
+
+            Debug.Log("Bug");
+        }
     }
 }
---
+++ Press Button/Assets/Scripts/Tracker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    
+    public bool SliderRevealed;
+    public bool CodeInserted;
+}

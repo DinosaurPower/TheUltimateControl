55d3ee52bb01879c1a5ac8b12157778a39f6f8a9
DinosaurPower <lysym685@newschool.edu>
2023-10-22 00:13:54 -0400

    Gate revealing script and password input

--- Press Button/Assets/Scripts/CodeInput.cs
+++ Press Button/Assets/Scripts/CodeInput.cs
@@ -7,13 +7,17 @@ public class CodeInput : MonoBehaviour
 { 
 public string password;
 public  TMP_InputField inputField;
-
+public GameObject[] FauxBG;
 
 
     // Start is called before the first frame update
    public void UpdatePassword(){
     if (inputField.text== password){
         Debug.Log("Access granted");
+        for (int i =0; i < FauxBG.Length; i++){
+        FauxBG[i].SetActive(false);
+        }
+        
     }
 
 
